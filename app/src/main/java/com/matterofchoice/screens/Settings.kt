5a44d893f53d5a460cb60d034316ddc24001a990
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-01-20 11:49:57 +0300

    It is all work good with improved UI

--- app/src/main/java/com/matterofchoice/screens/Settings.kt
+++ app/src/main/java/com/matterofchoice/screens/Settings.kt
@@ -1,7 +1,6 @@
 package com.matterofchoice.screens
 
 import android.content.Context
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
--- app/src/main/java/com/matterofchoice/ui/theme/Theme.kt
+++ app/src/main/java/com/matterofchoice/ui/theme/Theme.kt
@@ -1,21 +1,18 @@
 package com.matterofchoice.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+//private val DarkColorScheme = darkColorScheme(
+//    primary = Purple80,
+//    secondary = PurpleGrey80,
+//    tertiary = Pink80
+//)
 
 private val LightColorScheme = lightColorScheme(
     primary = MyColor,
