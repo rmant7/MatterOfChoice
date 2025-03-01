7f1bfc8a2ad9b159af2423d4862eaa60e024a9b5
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-03-01 16:51:53 +0300

    .

--- app/src/main/java/com/matterofchoice/viewmodel/AIViewModel.kt
+++ app/src/main/java/com/matterofchoice/viewmodel/AIViewModel.kt
@@ -37,7 +37,7 @@ class AIViewModel(application: Application) : AndroidViewModel(application) {
 
     private val model = GenerativeModel(
         modelName = "gemini-2.0-flash",
-        apiKey = "AIzaSyBbpQNYsB4bDDctAB14D8FQIIOqn7JOccc",
+        apiKey = "Your_api_key",
     )
 
     private val aiHistory by lazy {
